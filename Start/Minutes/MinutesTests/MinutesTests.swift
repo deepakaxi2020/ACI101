def9efbd5622b465ea86b099d5fe27adf4e2584f
Deepak Srivastava <deepak.srivastava@tabcorp.com.au>
2021-01-30 01:28:50 +0530

    Changes for title value added

---
+++ Start/Minutes/MinutesTests/MinutesTests.swift
@@ -0,0 +1,37 @@
+//
+//  MinutesTests.swift
+//  MinutesTests
+//
+//  Created by deepak Srivastava on 30/01/21.
+//  Copyright © 2021 Microsoft. All rights reserved.
+//
+
+
+import XCTest
+@testable import Minutes
+
+class MinutesTests: XCTestCase {
+
+    var entryUnderTest: Entry!
+
+    override func setUp() {
+        super.setUp()
+
+        entryUnderTest = Entry("Title", "Content")
+    }
+
+    override func tearDown() {
+        entryUnderTest = nil
+
+        super.tearDown()
+    }
+
+    func testSerialization() {
+        let serializedData = FileEntryStore.serialize(entryUnderTest)
+        let entry = FileEntryStore.deserialize(serializedData!)
+
+        XCTAssertEqual(entry?.title, "Title", "Title does not match")
+        XCTAssertEqual(entry?.content, "Content", "Content does not match")
+    }
+ }
+
